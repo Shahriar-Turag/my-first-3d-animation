965ccc247c76e7d890d06c185bb7254378b3a1cf
Shahria-Turag <shahriarturag.st@gmail.com>
2022-02-15 21:57:11 +0600

    final

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const container = document.querySelector(".container");
 //Items
 const title = document.querySelector(".title");
 const sneaker = document.querySelector(".sneaker img");
-const purchase = document.querySelector(".purchase");
+const purchase = document.querySelector(".purchase button");
 const description = document.querySelector(".info h3");
 const sizes = document.querySelector(".sizes");
 
 //Moving Animation Event
 container.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25 ;
   let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
